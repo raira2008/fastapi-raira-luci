6a22d7782f0f24e49c949262865bac19c43a236b
lucialves1991 <lucivaneawendell@gmail.com>
2025-08-25 22:40:45 +0000

    Rota dinâmica e recebimento de dados

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI(title = 'API da Raira')
 @app.get("/")
 def hello():
     return("message:" "Hello, world!")
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return {"o nome do aluno é": nome_aluno}
